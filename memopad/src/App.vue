d23a6aa22d4fc0312a8b97266779ce2f43f82508
irishkooy <marines.qvcmarin@gmail.com>
2022-04-24 02:24:49 +0900

    add: vuex

--- memopad/src/App.vue
+++ memopad/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
+    <router-link to="/new">New</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
@@ -11,12 +12,14 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  max-width: 600px;
+  margin: 0 auto;
 }
 
 nav {
   padding: 30px;
+  text-align: center;
 }
 
 nav a {
---
+++ memopad/src/views/NewView.vue
@@ -0,0 +1,13 @@
+<template>
+  <MemoForm />
+</template>
+
+<script>
+import MemoForm from '@/components/MemoForm.vue'
+export default {
+  name: 'NewVue',
+  components: {
+    MemoForm,
+  }
+}
+</script>
