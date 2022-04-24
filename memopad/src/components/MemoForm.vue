b81f1e9d3d0de26516002bf3b4626c3c83755f0f
irishkooy <marines.qvcmarin@gmail.com>
2022-04-24 15:59:05 +0900

    完成

--- memopad/src/components/MemoForm.vue
+++ memopad/src/components/MemoForm.vue
@@ -1,14 +1,44 @@
 <template>
-  <div><input type="text"></div>
-  <div><textarea></textarea></div>
+  <div><input type="text" v-model="title"></div>
+  <div><textarea v-model="content"></textarea></div>
   <div class="center">
-    <button>保存</button>
+    <button v-if="memo.id" @click="save">更新</button>
+    <button v-else @click="save">保存</button>
+    <button v-if="memo.id" @click="remove">削除</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'MemoForm'
+  name: 'MemoForm',
+  props: [
+    'memo'
+  ],
+  data() {
+    return {
+      title: this.memo.title,
+      content: this.memo.content,
+    }
+  },
+  methods: {
+    save() {
+      let memo = {
+        title: this.title,
+        content: this.content
+      }
+
+      if (this.memo.id) {
+        memo.id = this.memo.id
+      }
+
+      this.$store.commit('save', memo);
+      this.$router.push('/')
+    },
+    remove() {
+      this.$store.commit('delete', this.memo.id)
+      this.$router.push('/')
+    }
+  }
 }
 </script>
 
--- memopad/src/views/HomeView.vue
+++ memopad/src/views/HomeView.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul v-if="hasMemos">
+      <li v-for="memo in memos" :key="memo.id">
+        <router-link :to="{ name: 'edit', params: {id: memo.id }}">
+          {{ memo.title }}
+        </router-link>
+        <button @click="deleteMemo">削除</button>
+      </li>
+    </ul>
+    <p v-else>メモは有りません</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name: "HomeView",
+  computed: {
+    hasMemos() {
+      return this.$store.getters.getCount
+    },
+    memos() {
+      return this.$store.getters.getAll
+    }
+  },
+  methods: {
+    deleteMemo() {
+      const id = this.$store.memo.getters.getMemoById
+      this.$store.commit('delete', id)
+    }
   }
 }
 </script>
+
+<style scoped>
+ul {
+  margin: 0;
+  padding: 0;
+}
+li {
+  list-style: none;
+  border-bottom: 1px solid #ccc;
+  padding-bottom: 10px;
+  margin-bottom: 10px;
+}
+
+li a {
+  color: #999;
+  text-decoration: none;
+  width: 100%;
+  display: block;
+}
+</style>
